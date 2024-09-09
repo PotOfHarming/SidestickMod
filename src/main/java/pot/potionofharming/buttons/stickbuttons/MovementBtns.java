d1ad50527557879a42af8f239ffbd419ee98d889
PotionOfHarming <potofharming@gmail.com>
2024-09-09 21:00:15 +0200

    added movement

--- src/main/java/pot/potionofharming/buttons/stickbuttons/MovementBtns.java
+++ src/main/java/pot/potionofharming/buttons/stickbuttons/MovementBtns.java
@@ -1,22 +1,33 @@
 package pot.potionofharming.buttons.stickbuttons;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil.Key;
+
 public class MovementBtns {
+    private static boolean fw = false;
+    private static boolean bw = false;
+    private static boolean lf = false;
+    private static boolean rg = false;
     public static void movement(byte forward, byte right, byte backward, byte left) {
-//		if (forward!=1 || backward!=1) {
-//			if (forward == 1) {
-//				player.move(MovementType.PLAYER, new Vec3d(-player.getMovementSpeed(), 0, 0));
-//			}
-//			if (backward == 1) {
-//				player.move(MovementType.PLAYER, new Vec3d(player.getMovementSpeed(), 0, 0));
-//			}
-//		}
-//		if (right!=1 || left!=1) {
-//			if (left == 1) {
-//				player.move(MovementType.PLAYER, new Vec3d(0, 0, player.getMovementSpeed()));
-//			}
-//			if (right == 1) {
-//				player.move(MovementType.PLAYER, new Vec3d(0, 0, -player.getMovementSpeed()));
-//			}
-//		}
+        MinecraftClient client = MinecraftClient.getInstance();
+        Key fwk = client.options.forwardKey.getDefaultKey();
+        Key bwk = client.options.backKey.getDefaultKey();
+        Key lfk = client.options.leftKey.getDefaultKey();
+        Key rgk = client.options.rightKey.getDefaultKey();
+
+        fw = forward==1;
+        bw = backward==1;
+        lf = left==1;
+        rg = right==1;
+		if (!fw || !bw) {
+            KeyBinding.setKeyPressed(fwk, fw);
+            KeyBinding.setKeyPressed(bwk, bw);
+		}
+		if (!lf || !rg) {
+            KeyBinding.setKeyPressed(lfk, lf);
+            KeyBinding.setKeyPressed(rgk, rg);
+		}
     }
 }
+
